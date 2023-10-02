056d3609cf967fb150759e1849378e29e876be41
Rohit Paul <113459757+RohitPaul0007@users.noreply.github.com>
2023-10-02 21:13:02 +0530

    Update jscode.js

--- Clock/jscode.js
+++ Clock/jscode.js
@@ -13,17 +13,17 @@
       let month = now.getMonth();
       let year = now.getFullYear();
 
-      // STORE DAY AND MONTH NAME IN AN ARRAY
+      
       let dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
       let monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-      // FORMAT DATE AND TIME
+
       hours = hours % 12 || 12;
       minutes = minutes < 10 ? "0" + minutes : minutes;
       seconds = seconds < 10 ? "0" + seconds : seconds;
       date = date < 10 ? "0" + date : date;
 
-      // DISPLAY DATE AND TIME
+     
       let period = hours < 12 ? "AM" : "PM";
       time.innerHTML = hours + ":" + minutes + ":" + seconds + " " + period;
       dateTime.innerHTML = dayNames[day] + ", " + monthNames[month] + " " + date + ", " + year;
